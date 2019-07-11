04db284d147f7455fe3f067d32420f9c8945070d
ccoudsi <ccoudsi@coudsi.com>
2019-07-11 14:35:22 -0700

    modified server listening to cloud on app.js

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.get("/about", function(req, res){
 // ===================================================================
 // SERVER
 // ===================================================================
-app.listen(3000, function(){
-    console.log("Serving DepolyingDemo on port: 3000");
-});
+// app.listen(3000, function(){
+//     console.log("Serving DepolyingDemo on port: 3000");
+// });
+app.listen(process.env.PORT, process.env.IP);
